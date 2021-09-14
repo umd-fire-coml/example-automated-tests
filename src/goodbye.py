d88e6f00d2ed320a986c6044ab13d932eea7874e
Sagar Saxena <ssaxena1@umd.edu>
2021-09-14 11:50:05 -0400

    Add docstring to src files

--- src/goodbye.py
+++ src/goodbye.py
@@ -1,4 +1,10 @@
 class Goodbye:
 
     def getGoodbye():
+      """
+      Get the goodbye phrase.
+      
+      :return: the goodbye phrase
+      :rtype: str
+      """
       return "Goodbye World"
--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,10 @@
 class Hello:
 
     def getHello():
+      """
+      Get the hello phrase.
+      
+      :return: the hello phrase
+      :rtype: str
+      """
       return "Hello World"
