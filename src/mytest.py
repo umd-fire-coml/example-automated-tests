782a6f3ed59adb8ffaec7ca4eacd6d0414c93dc5
Alex Rubin <arubin12@terpmail.umd.edu>
2021-09-22 14:43:48 -0400

    "this is alex rubin"

---
+++ src/mytest.py
@@ -0,0 +1,7 @@
+class mytest:
+    def mytest():
+        "this is an example test that will return 1"
+        return 1;
+
+    
+    
---
+++ test/test_mytest.py
@@ -0,0 +1,5 @@
+from src.mytest import mytest
+
+def test_mytest():
+    assert(mytest.mytest == 1)
+
