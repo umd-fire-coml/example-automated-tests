9a17b79945bf6f219f0eb121d33d6a59f65cbc74
unknown <justinhl@umd.edu>
2021-09-22 14:36:22 -0400

    update :P

--- test/test_justin.py
+++ test/test_justin.py
@@ -1,4 +1,10 @@
 from src.justins_function import justins_function
 
-def test_get_justin():
-  assert(justins_function.add(10, 20) == "30")
+def test_justin1():
+  assert(justins_function.add(10, 20) == 30)
+
+def test_justin2():
+  assert(justins_function.add(5, 60) == 65)
+
+def test_justin3():
+  assert(justins_function.add(1, 1) == 2)
