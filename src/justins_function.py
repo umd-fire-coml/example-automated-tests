a57c0622a36fece7b3ac35adc4da10722e2d36cc
unknown <justinhl@umd.edu>
2021-09-22 14:30:50 -0400

    my work for in class activity

---
+++ src/justins_function.py
@@ -0,0 +1,3 @@
+class justins_function:
+	def add(x, y):
+		return x + y
---
+++ test/test_justin.py
@@ -0,0 +1,4 @@
+from src.justins_function import justins_function
+
+def test_get_justin():
+  assert(justins_function.add(10, 20) == "30")
